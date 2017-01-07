745ae4652745de16aa7bc28506ee398049c8c73e
Sammi Haj Hassine <sammi.hajhassine@gmail.com>
2017-01-07 18:48:13 +0100

    Fixed connection between app and API. Implemented image capture, loading spinner and displaying of result image

--- HappyOMeter/index.ios.js
+++ HappyOMeter/index.ios.js
@@ -4,18 +4,24 @@ import {
   Component,
   Dimensions,
   StyleSheet,
+  StatusBar,
+  Image,
   Text,
   TouchableHighlight,
   View
 } from 'react-native';
 import React from 'react';
+import axios from 'axios';
+
 import Camera from 'react-native-camera';
 import Button from 'react-native-button';
+import Spinner from 'react-native-loading-spinner-overlay';
+
 
 class HappyOMeter extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {takenPhotoURI: null};
+    this.state = {takenPhotoURI: null, loading: false};
   }
 
   setNativeProps(nativeProps) {
@@ -23,32 +29,52 @@ class HappyOMeter extends React.Component {
   }
 
   render() {
-    var content;
+    var content = (this.state.takenPhotoURI || this.state.loading) ? <Image
+          style={styles.preview}
+          source={{uri: `data:image/png;base64,${this.state.takenPhotoURI}`}}
+        /> :
+    <Camera
+      ref={(cam) => {
+        this.camera = cam;
+      }}
+      style={styles.preview}
+      captureTarget={Camera.constants.CaptureTarget.memory}
+      aspect={Camera.constants.Aspect.fill}>
+      <TouchableHighlight>
+        <View
+          ref={component => this._root = component}>
+          <Button containerStyle={styles.button} onPress={this.takePicture.bind(this)}></Button>
+        </View>
+      </TouchableHighlight>
+      </Camera>;
 
     return (
       <View style={styles.container}>
-      <Camera
-        ref={(cam) => {
-          this.camera = cam;
-        }}
-        style={styles.preview}
-        aspect={Camera.constants.Aspect.fill}>
-        <TouchableHighlight>
-          <View
-            ref={component => this._root = component}>
-            <Button containerStyle={styles.button} onPress={this.takePicture.bind(this)}></Button>
-          </View>
-        </TouchableHighlight>
-        </Camera>
+        <StatusBar hidden={true} />
+        <Spinner visible={this.state.loading} textStyle={{color: '#FFF'}} />
+        {content}
       </View>
     );
   }
 
   takePicture() {
+    const self = this;
     this.camera.capture()
       .then((data) => {
-        console.log(data);
-        this.setState({takenPhotoURI: data});
+        var config = {
+          headers: {'Content-Type': 'application/octet-stream;base64'}
+        };
+
+        self.setState({takenPhotoURI: data.data, loading: true});
+
+        axios.post('http://192.168.1.174:8080/emotions', data.data, config)
+        .then(function (response) {
+          self.setState({takenPhotoURI: response.data, loading: false});
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+
       })
       .catch(err => console.error(err));
   }
